a1b8d874e92baf647fd8a23951a4a09b7ac4a306
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2026-06-02 13:49:10 +0300

    uuu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v2026-06-02-1';
+const APP_VERSION = 'v2026-06-02-2';
 const CACHE_PREFIX = 'goldtech-pwa';
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${APP_VERSION}`;
